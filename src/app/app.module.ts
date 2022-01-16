c1a555144e3fcfd852ba94d2b92d0b40ad97f4ac
Anarbek <anarbek0akylbekuly@gmail.com>
2022-01-16 12:35:11 +0600

    material design has been installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
